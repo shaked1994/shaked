74535a8214a5d7a25639c515bee2a10fda697edf
shaked1994 <88157840+shaked1994@users.noreply.github.com>
2021-07-29 22:36:02 +0300

    Add files via upload

--- class.py
+++ class.py
@@ -21,6 +21,9 @@ ob5=Company('Github',random.randint(1,10),random.randint(1,10))
 
 ob_lst=[ob1,ob2,ob3,ob4,ob5]
 
+ob_original_derug=[ob1.company_name,ob1.worthness(),ob2.company_name,ob2.worthness(),ob3.company_name,ob3.worthness(),ob4.company_name,ob4.worthness(),ob5.company_name,ob5.worthness()]
+
+
 worthness1=ob1.worthness()
 worthness2=ob2.worthness()
 worthness3=ob3.worthness()
@@ -30,17 +33,38 @@ worthness5=ob5.worthness()
 worth_lst=[worthness1,worthness2,worthness3,worthness4,worthness5]
 worth_lst.sort()
 
-derug=[]
 
-for i in worth_lst:
+derug=[0,0,0,0,0]
 
-    for j in worth_lst
-    if i==ob_lst[i].worthness():
-        derug[i]=worth_lst[i]
-        derug[ob_lst[i].worthness()]
-        lst[i]=ob_lst[i].company_name
+for i in range(len(worth_lst)):
+    for j in range(len(ob_lst)):
+     if (worth_lst[i]==ob_lst[j].worthness()):
+          derug[i]=ob_lst[j].company_name
+          j=0
 
-     elif:
-      i=i+1
+
+print("The original company rating is:"+ str(ob_original_derug))
+print('worth list is', str(worth_lst))
+print('derug is',str(derug))
+
+for i in range (5):
+   if i==0:
+       print('The lowest rating belong to' ,str(derug[i]),'with the rate of' ,str( worth_lst[i]))
+
+   if i==1:
+       print("The second lowest rating belong to",str(derug[i]),"with the rate of" ,str(worth_lst[i]))
+
+   if i==2:
+        print("In the third place there's",str(derug[i]),"with the rate of", str(worth_lst[i]))
+
+   if i==3:
+        print("The second place belongs to",str(derug[i]),"with the rate of",str(worth_lst[i]))
+
+   if i==4:
+      print("And in the first place with the highest rating we have", str(derug[i]),"with the rate of", str(worth_lst[i]))
+
+
+
+
 
 
